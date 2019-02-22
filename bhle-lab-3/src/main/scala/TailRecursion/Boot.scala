f40e19dab0c2dd1cf1b6f4613eb22a37809c5495
GSedi <sedi.gamidov@gmail.com>
2019-02-22 22:28:08 +0600

    TailRecursion Task 2

--- bhle-lab-3/src/main/scala/TailRecursion/Boot.scala
+++ bhle-lab-3/src/main/scala/TailRecursion/Boot.scala
@@ -9,6 +9,11 @@ object Boot extends App {
       case End => cnt
       case Node(head, tail) => tail.length(cnt + 1)
     }
+
+    def product(acc: Int = 1): Int = this match {
+      case End => acc
+      case Node(head, tail) => tail.product(acc * head)
+    }
   }
   case object End extends IntList
   case class Node(head: Int, tail: IntList) extends IntList
@@ -19,4 +24,11 @@ object Boot extends App {
   assert(intList.tail.length() == 3)
   assert(End.length() == 0)
   println(intList.length())
+
+
+  // Task 2
+  assert(intList.product() == 1 * 2 * 3 * 4)
+  assert(intList.tail.product() == 2 * 3 * 4)
+  assert(End.product() == 1)
+  println(intList.product())
 }
